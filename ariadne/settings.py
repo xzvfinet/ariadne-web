fbdec3e6fb60af43888a2bc5913fad01cdb98b59
xzvfinet <xzvfinet@gmail.com>
2016-10-26 06:43:31 +0000

    add EB settings and requirements.txt

--- ariadne/settings.py
+++ ariadne/settings.py
@@ -73,7 +73,19 @@ WSGI_APPLICATION = 'ariadne.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+  DATABASES = {
       'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'ariadne',
@@ -82,8 +94,8 @@ DATABASES = {
             'HOST': 'localhost',
             'PORT': '5432',
       }
-}
-
+  }
+  
 
 
 # Password validation
@@ -122,4 +134,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
+
